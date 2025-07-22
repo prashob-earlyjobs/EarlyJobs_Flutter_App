7445da85d67af6f83611388c22bda2aa1e79629d
susmitkb <susmitkb@gmail.com>
2025-07-22 17:47:56 +0530

    go router services added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
-import 'package:earlyjobs/Model/jobsModel.dart';
-import 'package:earlyjobs/View/Home/homescreen.dart';
-import 'package:earlyjobs/View/Jobs/individualjob.dart';
-import 'package:earlyjobs/View/Jobs/jobs.dart';
-import 'package:earlyjobs/View/Jobs/jobsscreen.dart';
-import 'package:earlyjobs/View/splashscreen.dart';
+import 'package:earlyjobs/routes/router.config.dart' show EarlyJobsAppRoutes;
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'Constants/constants.dart';
 
 void main() {
@@ -18,7 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -33,44 +25,9 @@ class MyApp extends StatelessWidget {
             fontFamily: GoogleFonts.poppins().fontFamily,
             useMaterial3: true,
           ),
-          routerConfig: _router,
+          routerConfig: EarlyJobsAppRoutes.router,
         );
       },
     );
   }
 }
-
-final GoRouter _router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return SplashScreen();
-      },
-    ),
-    GoRoute(
-      path: '/homeScreen',
-      builder: (BuildContext context, GoRouterState state) {
-        return HomeScreen();
-      },
-    ),
-    GoRoute(
-      path: '/jobsScreen',
-      builder: (BuildContext context, GoRouterState state) {
-        return JobsScreen();
-      },
-    ),
-    GoRoute(
-      path: '/individualJobs',
-      builder: (BuildContext context, GoRouterState state) {
-        JobsModel jobData = state.extra as JobsModel;
-        return IndividualJob(
-          jobData: jobData,
-        );
-      },
-    ),
-  ],
-  // errorPageBuilder: (context, state) {
-  //   return const MaterialPage(child: ErrorPage());
-  // },
-);
